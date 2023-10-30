5f26bb28f5ae59f327dc4e7a912deec86f1c3144
Abdessamad Nouri <nouri.abde.dev@gmail.com>
2023-10-30 17:49:21 +0000

    chore: remove unnecessary comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { Model } from './Shoe'
 
 export default function App() {
-  console.log("in app")
   return (
     <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
       <Environment preset="forest" />
